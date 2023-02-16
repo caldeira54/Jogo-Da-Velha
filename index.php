5a159d0f5b62d78fb3790852f7c7885ae04157e9
ARTHUR ALVES CALDEIRA <0057842@IFMG.EDU.BR>
2023-02-16 08:34:27 -0300

    Ajustes

--- index.php
+++ index.php
@@ -5,42 +5,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="./css/estilo.css"> -->
     <title>Jogo da Velha</title>
 </head>
 
-<body>
-    <div class="div">
-        <form action="./formCadastro.php">
-            <div class="retangulo">
-                <div class="label">
-                    <label for="texto"> Cadastrar um novo jogador </label>
-                </div>
+<body class="bg-light">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <form action="./formCadastro.php">
+                    <div>
+                        <label for="texto" class="text-dark p-3"> Cadastrar um novo jogador </label>
+                    </div>
 
-                <button> Cadastrar </button>
+                    <button class="btn btn-outline-dark"> Cadastrar </button>
+                </form>
             </div>
-        </form>
-        
-        <form action="./formDefine.php">
-            <div class="retangulo">
-                <div class="label">
-                    <label for="texto"> Definir Jogadores </label>
-                </div>
 
-                <button> Definir </button>
+            <div class="col">
+                <form action="./formDefine.php">
+                    <div class="label">
+                        <label for="texto" class="text-dark p-3"> Definir Jogadores </label>
+                    </div>
+
+                    <button class="btn btn-outline-dark"> Definir </button>
+                </form>
             </div>
-        </form>
 
-        <form action="./listagem.php">
-            <div class="retangulo">
-                <div class="label">
-                    <label for="texto"> Lista de Jogadores </label>
-                </div>
+            <div class="col">
+                <form action="./listagem.php">
+                    <div class="label">
+                        <label for="texto" class="text-dark p-3"> Lista de Jogadores </label>
+                    </div>
 
-                <button> Listar </button>
+                    <button class="btn btn-outline-dark"> Listar </button>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
