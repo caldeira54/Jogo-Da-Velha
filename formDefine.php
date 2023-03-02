d23159db832d13975eacd06098c23923ec8282a2
caldeira54 <caldeiraa54@gmail.com>
2023-03-02 12:11:56 -0300

    Ajustes

--- formDefine.php
+++ formDefine.php
@@ -5,17 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="./css/estilo.css"> -->
     <title>Definir Jogadores</title>
 </head>
 
 <body>
     <div class="div">
-        <div class="retangulo">
+        <div class="container-fluid">
             <div>
                 <form action="tabuleiro.php" method="POST">
                     <label for="jogador1">Jogador 1</label>
-                    <select class="dados" name="jogador1" id="jogador1">
+                    <select class="form-select" name="jogador1" id="jogador1">
                         <?php
                         session_start();
 
@@ -36,7 +37,7 @@
                     <br>
 
                     <label for="jogador2">Jogador 2</label>
-                    <select class="dados" name="jogador2" id="jogador2">
+                    <select class="form-select" name="jogador2" id="jogador2">
                         <?php
                         require_once './dao/DAOJogador.php';
                         require_once './dao/Conexao.php';
@@ -52,19 +53,24 @@
                         ?>
                     </select>
 
-                    <div class="button">
-                        <button> Jogar </button>
+                    <br>
+
+                    <div class="container-fluid">
+                        <button class="btn btn-outline-dark"> Jogar </button>
                     </div>
                 </form>
             </div>
 
-            <div class="botoes">
+            <br>
+            <div class="container-fluid">
                 <form action="./index.php">
-                    <button> Início </button>
+                    <button class="btn btn-outline-dark"> Início </button>
                 </form>
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
